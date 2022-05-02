288106a5df856c6657c674960da643ab5f8aaa8b
ElDracoUniversitario <91895033+ElDracoUniversitario@users.noreply.github.com>
2022-05-02 12:22:33 +0200

    Update bucle.sh

--- PruebasFB0/bucle.sh
+++ PruebasFB0/bucle.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 for f in *.png; do
-  if $f != "negro.png"
-  then
+  if ["$f" != "negro.png"]; then
     pngtopnm $f | ./ppmtorgb565 > /dev/fb0
     sleep 5s
     pngtopnm negro.png | ./ppmtorgb565 > /dev/fb0
